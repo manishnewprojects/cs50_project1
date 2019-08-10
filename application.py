4a436a357d34ab21de2f1e3118c782bf1f29dc96
NewProjects <newprojects@manishs-mbp.lan>
2019-08-10 09:28:19 -0700

    Working through step 5 + Goodreads

--- application.py
+++ application.py
@@ -3,12 +3,14 @@ import os
 from flask import Flask, session, render_template, request, flash 
 from flask import redirect, session, abort, url_for,session,logging,request 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import or_, and_
 
+import requests
 
 app = Flask(__name__)
+app.config['GOODREADS_KEY'] = '6Ck8APh3scMY0Ix0NDcA'
 app.config['SESSION_TYPE'] = 'filesystem'
 app.secret_key = os.urandom(12)
-app.config['FLASK_DEBUG']=1 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ktvjqnlkicstdh:12d3502af6b551ee281300b1a3fe36f6646c953d9eeeaf44b25afde404c29b65@ec2-174-129-41-127.compute-1.amazonaws.com:5432/d1r9eob107ife4'
 db = SQLAlchemy(app)
@@ -21,12 +23,22 @@ class user(db.Model):
 	user_password = db.Column(db.String(80))
 
 
+class book_data(db.Model):
+	__tablename__ = "book_info"
+	title 		  = db.Column(db.String(120))
+	author 	 	  = db.Column(db.String(120))
+	isbn          = db.Column(db.String(80), unique=True, primary_key=True)
+	year          = db.Column(db.String(80))
+	review_count  = db.Column(db.Integer)
+	average_score = db.Column(db.Integer)
+
+
 @app.route("/")
 def index():
 	if not session.get('logged_in'):
 		return render_template("index.html")
 	else:
-		return redirect(url_for('homepage', username = session.get('username')))
+		return render_template("homepage.html", user = session.get('username'))
 
 
 
@@ -43,8 +55,9 @@ def login():
 		else:
 			session['logged_in'] = True
 			session['username']  = uname 
-			return redirect(url_for('homepage', username = uname))
+			return render_template("homepage.html", user = uname)
 	return render_template("login.html")
+
 
 @app.route("/register", methods=["GET","POST"])
 def register():
@@ -68,17 +81,38 @@ def register():
 	return render_template("register.html", error = error)
 
 
+
+@app.route('/search', methods=["GET","POST"])
+def search():
+ 	if request.method == "POST":
+	  	search_term=request.form.get('search_term')
+ 	 	
+ 	 	search_string = "%{}%".format(search_term)
+		search_results = book_data.query.filter(or_ (book_data.author.ilike(search_string), book_data.isbn.like(search_string), book_data.title.ilike(search_string)) ).all()
+		 
+
+		if not search_results:
+			flash('No results found!')
+			return render_template("search.html")
+		else:
+		# display results
+			return render_template("search.html", search_results=search_results)
+
+	return render_template("search.html")
+
+@app.route('/api/<isbn>', methods=["GET","POST"])
+def api(isbn):
+	print("got ISBN",isbn)
+	goodreads_results = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "6Ck8APh3scMY0Ix0NDcA", "isbns": isbn})
+	print(goodreads_results.json())
+
+	return render_template("search.html")
+
+
 @app.route("/logout")
 def logout():
 	session['logged_in'] = False
 	return index()
 
-@app.route("/homepage/<username>", methods=["GET", "POST"])
-def homepage(username):
-	return "Hello " +username+ "! Welome Back! Enjoy the site  <a href=\"/logout\">Logout</a>"
-
-
-if __name__ == "__main__":
-	app.run(debug=True,host='0.0.0.0', port=5000)
-
-
+ 
+ 
